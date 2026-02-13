81184198d69905d80ca4ad1a4eb5a95bc81a29e8
tony-hut <anton.hut.dev@gmail.com>
2026-02-14 00:33:48 +0200

    Refactor: Switch to EJS template engine

--- index.js
+++ index.js
@@ -8,52 +8,34 @@ import QRCode from "qrcode";
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/submit", (req, res) => {
-  const userUrl = req.body.url;
+  const url = req.body.url;
   const qrColor = req.body.color || "#000000";
 
+  if (!url) return res.render("index", { qrCode: null });
+
   QRCode.toDataURL(
-    userUrl,
+    url,
     {
       color: {
-        dark: qrColor, 
-        light: "#ffffff", 
+        dark: qrColor,
+        light: "#00000000",
       },
     },
     (err, imageCode) => {
-      if (err) {
-        res.send("Сталася помилка");
-        return;
-      }
-      res.send(`
-    <!DOCTYPE html>
-      <html>
-      <head>
-        <title>QR Code Ready</title>
-        <link rel="stylesheet" href="/style.css"> </head>
-      <body>
-        <div class="container"> <h1>Ваш QR готовий!</h1>
-          
-          <img src="${imageCode}" alt="QR Code" style="width: 200px; margin: 20px 0;">
-          
-          <br>
-          
-          <a href="/">
-            <button>Створити ще один</button>
-          </a>
-        </div>
-      </body>
-      </html>
-    `);
+      if (err) return res.send("Сталася помилка");
+
+      res.render("index", { qrCode: imageCode });
     },
   );
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.render("index", { qrCode: null });
 });
 
 app.listen(port, () => {
